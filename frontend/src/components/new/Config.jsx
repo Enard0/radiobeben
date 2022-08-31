58a5e5091b5c419b5fc704318900bded681f7d46
ultimacrom <ultimacrom@outlook.com>
2022-08-31 12:22:31 +0200

    new schedule test

--- frontend/src/components/new/Config.jsx
+++ frontend/src/components/new/Config.jsx
@@ -13,19 +13,105 @@ export default class ConfigPanel extends React.Component {
         return (
             <div className="allsuggestionspanel">
                 {this.state.upr_schedule ? <Breakes/> : null}
-                {this.props.schedule}
-                {this.state.upr_schedule}
             </div>
         );
     }
 };
 
 class Breakes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fetched: false,
+            data: []
+        }
+    }
+
+    get_data = async () => {
+        const r = await fetch('/api/playlist/weekdays', {
+            method: 'GET',
+        });
+        if (r.ok) {
+            this.setState({ data: JSON.parse(await r.text()) });
+        } else {
+            console.log(await r.text());
+        }
+
+        const r2 = await fetch('/api/playlist/breaktimes', {
+            method: 'GET',
+        });
+        if (r2.ok) {
+            this.setState({ fetched: true, presets: JSON.parse(await r2.text()) });
+        } else {
+            console.log(await r2.text());
+        }
+
+    }
+
+    componentDidMount() {
+        this.get_data();
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        let changed = false;
+        for (let i = 0; i < 7; i++) {
+            let weekday = (i + 1) % 7;
+            let data = {
+                weekday: weekday,
+                isEnabled: event.target.elements[i + "_enabled"].checked,
+                breaktimeid: parseInt(event.target.elements[i + "breaktime"].value),
+                visibility: parseInt(event.target.elements[i + "_visibility"].value)
+            }
+            if (data.isEnabled !== this.state.data[weekday].isEnabled
+                || !this.state.data[weekday].breaketime
+                || data.breaktimeid !== this.state.data[weekday].breaketime.id 
+                || data.visibility !== this.state.data[weekday].visibility) {
+                // need to send data to sever
+                const r = await fetch('/api/playlist/schedule', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data)
+                });
+                if (r.ok) {
+                    changed = true;
+
+                } else {
+                    console.log(await r.text());
+                }
+            }
+        }
+        if (changed) {
+            // somehow refresh playlist 
+        }
+    }
+
     render() {
+        const days = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
+        let toRender = [];
+        if (this.state.fetched) {
+            for (let i = 0; i < 7; i++) {
+                let weekday = (i + 1) % 7; // js bad, sunday is 0
+                toRender.push(<Weekday
+                    key={i}
+                    number={i}
+                    visibility={this.state.data[weekday].visibility}
+                    enabled={this.state.data[weekday].isEnabled}
+                    name={days[i]}
+                    breaketime={this.state.data[weekday].breaketime ? this.state.data[weekday].breaketime.id : null}
+                    presets={this.state.presets}
+                />)
+            }
+        }
         return (
             <div>
-                test
+                <form onSubmit={this.handleSubmit}>
+                    {toRender}
+                    <button type="submit">potwierdź</button>
+                </form>
             </div>
-        );
+        )
     }
-};
+}
