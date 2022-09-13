800a3636e263f75ea4745ecc3837818937b77aa4
Enard0 <50167741+Enard0@users.noreply.github.com>
2022-09-13 14:11:49 +0200

    Test start

--- backend/src/player/player.ts
+++ backend/src/player/player.ts
@@ -63,7 +63,11 @@ export default class player {
     }
     private start = (song: Song, from_playlist?: boolean, id?: number) => {
         console.log("starting")
-        this.ffplay = spawn(process.env.FFPLAY_PATH || "ffplay", ["-nodisp", "-autoexit", join(cfg.song_folder, song.filename)], {stdio: 'ignore'});
+        if (song.start=0){
+            this.ffplay = spawn(process.env.FFPLAY_PATH || "ffplay", ["-nodisp", "-autoexit", join(cfg.song_folder, song.filename)], {stdio: 'ignore'});
+        } else {
+            this.ffplay = spawn(process.env.FFPLAY_PATH || "ffplay", ["-nodisp", "-autoexit", "-ss"+song.start, join(cfg.song_folder, song.filename)], {stdio: 'ignore'});
+        }
         this.song = song;
         if (from_playlist) {
             this.from_playlist = true;
